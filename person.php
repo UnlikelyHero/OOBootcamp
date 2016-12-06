3be41c86eae2d5d6991d470828885eca512627b9
UnlikelyHeroTx <dvida3@wgu.edu>
2016-12-06 13:33:31 -0600

    Episode 02: Getters and Setters
    In preparation of learning "Encapsulation", Jeffrey teaches us how to
    use getters (getValue) functions and setters (setValue) functions, and
    using them to include behaviors when setting properties.

---
+++ person.php
@@ -0,0 +1,31 @@
+<?php
+
+class Person {
+  public $name;
+  public $age;
+
+  public function __construct($name) {
+    $this->name = $name;
+
+  }
+
+  public function getAge() {
+    return $this->age * 365;
+
+  }
+
+  public function setAge($age) {
+    if ($age < 18) {
+      throw new Exception("Person is under age");
+    }
+
+    $this->age = $age;
+  }
+}
+
+$john = new Person('John Doe');
+$john->setAge(34);
+
+var_dump($john->getAge());
+
+ ?>
